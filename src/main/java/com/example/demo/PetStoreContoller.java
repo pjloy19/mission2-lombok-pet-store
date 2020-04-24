c716c31a2459eb83c5b36618b8b1829761fd992d
pjloy <supitchayang19@gmail.com>
2020-04-24 16:38:53 +0700

    Updated to java 11 Remove redundant class PetTest.java Change path name

--- src/main/java/com/example/demo/PetStoreContoller.java
+++ src/main/java/com/example/demo/PetStoreContoller.java
@@ -7,38 +7,37 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
-
 @RestController
 public class PetStoreContoller {
     @Autowired //Auto wired Bean dependency thru setter
     private PetStoreService petStoreService;
 
     //return all
-    @RequestMapping("/pet-store")
+    @RequestMapping("/pet")
     public List<PetStoreModel> getPetStore(){
         return petStoreService.getPetStoreModelList();
     }
 
     //return when passing id
-    @RequestMapping("/pet-store/{id}")
+    @RequestMapping("/pet/{id}")
     public PetStoreModel getPet(@PathVariable("id") String id){
         return petStoreService.getPet(id);
     }
 
     //POST data
-    @RequestMapping(method = RequestMethod.POST, value = "/pet-store")
+    @RequestMapping(method = RequestMethod.POST, value = "/pet")
     public void addPet(@RequestBody PetStoreModel pet){
         PetStoreService.addPet(pet);
     }
 
     //PUT data
-    @RequestMapping(method = RequestMethod.PUT, value = "/pet-store/{id}")
+    @RequestMapping(method = RequestMethod.PUT, value = "/pet/{id}")
     public void updatePet(@RequestBody PetStoreModel pet, @PathVariable("id") String id){
         PetStoreService.updatePet(pet, id);
     }
 
     //DEL data
-    @RequestMapping(method = RequestMethod.DELETE, value = "/pet-store/{id}")
+    @RequestMapping(method = RequestMethod.DELETE, value = "/pet/{id}")
     public void deletePet(@PathVariable("id") String id){
         PetStoreService.deletePet(id);
     }
